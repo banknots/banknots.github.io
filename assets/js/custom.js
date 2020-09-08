0d78caa23d12f1666dc3d6cce88d72d00665fa24
dev <user@users.noreply.github.com>
2020-09-08 14:11:36 +0300

    345

--- assets/js/custom.js
+++ assets/js/custom.js
@@ -1,41 +1,24 @@
-jQuery.extend(jQuery.fn, {
-	toplinkwidth: function(){
-		var totalContentWidth = jQuery('#content').outerWidth(); // ширина блока с контентом, включая padding
-		var totalTopLinkWidth = jQuery(this).children('a').outerWidth(true); // ширина самой кнопки наверх, включая padding и margin
-		var h = jQuery(window).width()/2-totalContentWidth/2-totalTopLinkWidth;
-		if(h<0){
-			// если кнопка не умещается, скрываем её
-			jQuery(this).hide();
-			return false;
-		} else {
-			if(jQuery(window).scrollTop() >= 1){
-				jQuery(this).show();
-			}
-			jQuery(this).css({'padding-right': h+'px'});
-			return true;
-		}
-	}
-});
- 
-jQuery(function($){
-	var topLink = $('#top-link');
-	topLink.css({'padding-bottom': $(window).height()});
-	// если вам не нужно, чтобы кнопка подстраивалась под ширину экрана - удалите следующие четыре строчки в коде
-	topLink.toplinkwidth();
-	$(window).resize(function(){
-		topLink.toplinkwidth();
-	});
-	$(window).scroll(function() {
-		if($(window).scrollTop() >= 1 && topLink.toplinkwidth()) {
-			topLink.fadeIn(300);
-		} else {
-			topLink.fadeOut(300);
-		}
-	});
-	topLink.click(function(e) {
-		$("body,html").animate({scrollTop: 0}, 500);
-		return false;
-	});
-});
- 
- 
+if ($(window).width() >= 1210) {
+      $('body').prepend("<a name='top' />");
+      $('body').append("<div id='go-top'><div id='go-top-text'>Наверх</div></div>")
+      $(window).scroll(function(){
+        if ($(document).scrollTop() > 200) {
+          $('#go-top').fadeIn();
+        } else {
+          $('#go-top').fadeOut();
+        }
+      });
+      $('#go-top').click(function(){
+        $("html, body").animate({
+          scrollTop: 0
+        }, 600);
+        return false;
+      });
+    }
+    $(window).resize(function() {
+      if ($(window).width() < 1210) {
+        $('#go-top').css({
+          'display':'none'
+        });
+      }
+    });
